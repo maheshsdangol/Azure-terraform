5d0101799b9d30e0224996190e5cf1010bf5e34b
root <root@ip-172-31-87-235.ec2.internal>
2020-12-17 06:04:16 +0000

    terrafrom update on azure-pipeline.yml

--- modules/azuremodule/main.tf
+++ modules/azuremodule/main.tf
@@ -1,3 +1,15 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rf_blobstore"
+        storage_account_name = "tfstorageaccount1"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+ 	subscription_id      = "338fe939-8530-4e88-a944-fbeda4421ddd"
+        tenant_id            = "88fdd5f5-af3e-46af-91a4-8a77fa86e04e"
+  }
+}
+
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "eastus"
